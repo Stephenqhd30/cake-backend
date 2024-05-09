f1b2303217d5d6f2f0f9de867b17801390b80623
Stephenqhd30 <Popcornqhd@gmail.com>
2024-05-09 16:09:35 +0800

    fixed 修复头像上传的功能

--- src/main/java/com/stephen/popcorn/model/enums/FileUploadBizEnum.java
+++ src/main/java/com/stephen/popcorn/model/enums/FileUploadBizEnum.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import lombok.Getter;
 import org.apache.commons.lang3.ObjectUtils;
 
 /**
@@ -11,6 +12,7 @@ import org.apache.commons.lang3.ObjectUtils;
  *
  * @author stephen qiu
  */
+@Getter
 public enum FileUploadBizEnum {
 	
 	USER_AVATAR("用户头像", "user_avatar");
@@ -51,11 +53,4 @@ public enum FileUploadBizEnum {
 		return null;
 	}
 	
-	public String getValue() {
-		return value;
-	}
-	
-	public String getText() {
-		return text;
-	}
 }
