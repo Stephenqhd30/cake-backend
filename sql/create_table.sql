7c1b0f15cac3db4d9379bb5b8b2fafc25dbd8e98
Stephenqhd30 <Popcornqhd@gmail.com>
2024-05-13 15:10:42 +0800

    change 修改用户表的字段

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -9,22 +9,29 @@ create database if not exists my_db;
 use my_db;
 
 -- 用户表
-create table if not exists user
+create table user
 (
-    id           bigint auto_increment comment 'id' primary key,
+    id           bigint auto_increment comment 'id'
+        primary key,
     userAccount  varchar(256)                           not null comment '账号',
     userPassword varchar(512)                           not null comment '密码',
     unionId      varchar(256)                           null comment '微信开放平台id',
     mpOpenId     varchar(256)                           null comment '公众号openId',
     userName     varchar(256)                           null comment '用户昵称',
+    userPhone    varchar(256)                           null comment '手机号码',
     userAvatar   varchar(1024)                          null comment '用户头像',
     userProfile  varchar(512)                           null comment '用户简介',
     userRole     varchar(256) default 'user'            not null comment '用户角色：user/admin/ban',
+    userEmail    varchar(256)                           null comment '用户邮箱',
     createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
     updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete     tinyint      default 0                 not null comment '是否删除',
-    index idx_unionId (unionId)
-) comment '用户' collate = utf8mb4_unicode_ci;
+    isDelete     tinyint      default 0                 not null comment '是否删除'
+)
+    comment '用户' collate = utf8mb4_unicode_ci;
+
+create index idx_unionId
+    on user (unionId);
+
 
 -- 帖子表
 create table if not exists post
